d4384c9d2e2b7a95ea5b4bd20b117fcafccbed0e
Your NameRicardo <ricardin.germanoo@gmail.com>
2021-05-08 06:40:16 -0300

    Projeto Finalizado

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using ENTREVISTA_EMPREGARE.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ENTREVISTA_EMPREGARE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var listaVagas = new List<Vaga>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                listaVagas.Add(new Vaga()
+                {
+                    Titulo = "Vendedor Externo",
+                    Localizacao = "São José do Rio Preto",
+                    Salario = 2000
+                });
+            }
+
+            return View(listaVagas);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Vaga.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ENTREVISTA_EMPREGARE.Models
+{
+    public class Vaga
+    {
+        public string Titulo { get; set; }
+        public string Localizacao { get; set; }
+        public decimal Salario { get; set; }
+
+
+    }
+}
+
